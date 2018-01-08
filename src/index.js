e1a44fc94b73709cdead5b3ad8f9fd3c38652712
github username <github email address>
2018-01-07 23:45:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img src="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="Mad Max: Fury Road" />
+        <h2>Mad Max: Fury Road</h2>
+        <small>Genres: Action, Adventure, Science Fiction, Thriller</small>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <MovieCard />,
+  document.getElementById('root')
+);
